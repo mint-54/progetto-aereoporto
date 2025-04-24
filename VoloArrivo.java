07a38250bead956608bbc8722c937ea8c0181100
mint-54 <146209965+mint-54@users.noreply.github.com>
2025-04-25 00:42:29 +0200

    Delete VoloArrivo.java

--- VoloArrivo.java
+++
@@ -1,9 +0,0 @@
-import java.time.LocalDateTime;
-
-public class VoloArrivo extends Volo {
-    String Destinazione;
-        public VoloArrivo(String idvolo, String CompagniaAerea, String OrigineAereoporto, double OrarioRitardo, StatoVolo stato, Utente chiamante) {
-            super(idvolo, CompagniaAerea, OrigineAereoporto, OrarioRitardo, stato, chiamante);
-            this.Destinazione = "Napoli";
-        }
-    }
